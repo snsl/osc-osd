29f00d78f4ff9fe1ce9ce8552737b866e822471c
Paul Betts <paul.betts@gmail.com>
2006-11-13 21:36:53 +0000

    Fixed automake stuff, got write-test program to compile correctly; switched libosd to automake because it will be easier to do stuff with it later

--- osd_initiator/libosd/write.c
+++ osd_initiator/libosd/write.c
@@ -1,8 +1,27 @@
 /*
  * Test the chardev transport to the kernel.
  */
+
+#ifndef __KERNEL__
+typedef signed char s8;
+typedef unsigned char u8;
+
+typedef signed short s16;
+typedef unsigned short u16;
+
+typedef signed int s32;
+typedef unsigned int u32;
+
+typedef signed long long s64;
+typedef unsigned long long u64;
+
+#define BITS_PER_LONG 32 
+
+#endif
+
 #include <stdio.h>
 #include <string.h>
+#include <linux/types.h>
 #include <scsi/scsi.h>
 #include "interface.h"
 #include "util.h"

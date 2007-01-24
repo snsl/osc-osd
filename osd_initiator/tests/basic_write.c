ec84ca777a0910a50fa7cdd8fd28ff468c208532
Pete Wyckoff <pw@osc.edu>
2007-01-24 22:57:52 +0000

    old git: add some read/write examples
    Signed-off-by: Boaz Harrosh <bharrosh@panasas.com>

--- osd_initiator/tests/basic_write.c
+++ osd_initiator/tests/basic_write.c
@@ -37,7 +37,11 @@ typedef unsigned long long u64;
  */
 static void varlen_cdb_init(uint8_t *cdb)
 {
-	memset(cdb, 0, VARLEN_CDB_SIZE);
+	int i;
+
+	/* memset(cdb, 0, VARLEN_CDB_SIZE); */
+	for (i=0; i<VARLEN_CDB_SIZE; i++)
+		cdb[i] = i;
 	cdb[0] = VARLEN_CDB;
 	cdb[7] = VARLEN_CDB_SIZE - 8;  /* total length */
 	cdb[11] = 2 << 4;  /* get/set attributes page format */
@@ -79,6 +83,7 @@ int main(int argc, char *argv[])
 
 	fd = uosd_open("/dev/sua");
 
+#if 0
 	for (i=0; i<2; i++)
 	{
 		info("inquiry");
@@ -102,19 +107,43 @@ int main(int argc, char *argv[])
 	varlen_cdb_init(cdb);
 	osd_flush_osd(cdb, 2);   /* flush everything */
 	uosd_cdb_nodata(fd, cdb, 200);
+	info("waiting for response");
+	err = uosd_wait_response(fd, &key);
+	info("response key %lx error %d", key, err);
 
 	info("sleeping 2 before format");
 	sleep(2);
+#endif
 
 	info("osd format osd");
 	varlen_cdb_init(cdb);
 	osd_format_osd(cdb, 1<<30);   /* capacity 1 GB */
 	uosd_cdb_nodata(fd, cdb, 200);
-
-	/* now wait for the result */
-	info("waiting for response");
 	err = uosd_wait_response(fd, &key);
 	info("response key %lx error %d", key, err);
+
+	info("osd create");
+	varlen_cdb_init(cdb);
+	osd_create(cdb, 0, 27, 1);
+	uosd_cdb_nodata(fd, cdb, 200);
+	err = uosd_wait_response(fd, &key);
+	info("response key %lx error %d", key, err);
+
+	const char buf[] = "The rain in spain";
+	info("osd write");
+	varlen_cdb_init(cdb);
+	osd_write(cdb, 0, 27, 10, 5);
+	uosd_cdb_write(fd, cdb, 200, buf, 10);
+	err = uosd_wait_response(fd, &key);
+	info("response key %lx error %d", key, err);
+
+	char bufout[] = "xxxxxxxxxxxxxxxxx";
+	info("osd read");
+	varlen_cdb_init(cdb);
+	osd_read(cdb, 0, 27, 10, 5);
+	uosd_cdb_read(fd, cdb, 200, bufout, 10);
+	err = uosd_wait_response(fd, &key);
+	info("response key %lx error %d, bufout %s", key, err, bufout);
 
 	uosd_close(fd);
 	return 0;

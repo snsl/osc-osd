e135bc4045771b257844496a02816510284c1904
Paul Betts <paul.betts@gmail.com>
2007-02-07 22:27:49 +0000

    old git: make all script now supports clean/all/etc
    Signed-off-by: Boaz Harrosh <bharrosh@panasas.com>

--- make_all.sh
+++ make_all.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 cd kernel
-make || exit 1
+make $@ 
 cd ../util
-make || exit 1
+make $2 || exit 1
 cd ../osd_initiator
 ./autogen.sh --prefix=/usr/local || exit 1
-make || exit 1
+make $@ || exit 1
 cd ../osd-target
-make || exit 1
+make $@ 

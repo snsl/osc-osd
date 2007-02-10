3c7113898469aa333b7a05a507460591ed724cb3
Pete Wyckoff <pw@osc.edu>
2007-02-10 19:52:28 +0000

    old git: Rename to point out this is just a C-like file, and add a comment.
    Signed-off-by: Boaz Harrosh <bharrosh@panasas.com>

---
+++ osd_initiator/python/libosdpython.h
@@ -0,0 +1,8 @@
+/*
+ * Lines that look like #include will be processed with CPP to produce struct
+ * and function declarations to be included in the python interface.
+ */
+#include <stddef.h>
+#include <stdint.h>
+#include "../interface.h"
+#include "../diskinfo.h"

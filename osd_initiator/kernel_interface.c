199a77d83927b1af1da99bf4d268940bae4fe510
Timothy Arnold <arnold.604@osu.edu>
2007-02-20 00:08:51 +0000

    old git: made the read command work
    Signed-off-by: Boaz Harrosh <bharrosh@panasas.com>

--- osd_initiator/kernel_interface.c
+++ osd_initiator/kernel_interface.c
@@ -220,17 +220,23 @@ int osd_sgio_wait_response(int fd, struct osd_command **out_command)
 	command->status = sg.device_status;
 	command->sense_len = sg.response_len;
 #else
-	/*
+/*	
 	printf("%s: status %u sense len %u resid %d\n", __func__, sg.status,
 	       sg.sb_len_wr, sg.resid);
-	*/
+*/	
 	command = sg.usr_ptr;
 
+	/* command->inlen_alloc === sg.resid, so the following command
+	   never gets a chance to execute the way it was--the data
+	   was never displayed as 0 was assigned to command->inlen   */
 	if (command->inlen_alloc)
-		command->inlen = command->inlen_alloc - sg.resid;
+		/* command->inlen = command->inlen_alloc - sg.resid; */
+		command->inlen = sg.resid;
+
 	command->status = sg.status;
 	command->sense_len = sg.sb_len_wr;
 #endif
+	
 
 	*out_command = command;
 

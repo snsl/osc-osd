be2f42750dc81d2bdf94de99cf439aebb44ba8c9
Pete Wyckoff <pw@osc.edu>
2007-06-16 23:30:10 +0000

    old git: Go to 512k, skip the sync runs.
    Signed-off-by: Boaz Harrosh <bharrosh@panasas.com>

--- osd_initiator/tests/iospeed.c
+++ osd_initiator/tests/iospeed.c
@@ -214,18 +214,20 @@ int main(int argc, char *argv[])
 
 	printf("# osd_initiator/tests/iospeed\n");
 	printf("# type  size (kB)  rate (MB/s) +- stdev\n");
-	for (i=4096; i<=262144; i+=4096)
+	for (i=4096; i<=(1<<19); i+=4096)
 		write_bw(fd, PARTITION_PID_LB, oid, i, iter, 0);
 	printf("\n\n");
-	for (i=4096; i<=262144; i+=4096)
+	for (i=4096; i<=(1<<19); i+=4096)
 		read_bw(fd, PARTITION_PID_LB, oid, i, iter, 0);
 	printf("\n\n");
-	for (i=4096; i<=262144; i+=4096)
+#if 0
+	for (i=4096; i<=(1<<19); i+=4096)
 		write_bw(fd, PARTITION_PID_LB, oid, i, iter, 1);
 	printf("\n\n");
-	for (i=4096; i<=262144; i+=4096)
+	for (i=4096; i<=(1<<19); i+=4096)
 		read_bw(fd, PARTITION_PID_LB, oid, i, iter, 1);
 	printf("\n\n");
+#endif
 
 	obj_remove(fd, PARTITION_PID_LB, oid);
 

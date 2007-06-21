cf09c71ebef4a2bc2eed8a9964518874e45ff254
Dennis Dalessandro <dennis@osc.edu>
2007-06-21 19:47:01 +0000

    old git: make suitable for a batch job
    Signed-off-by: Boaz Harrosh <bharrosh@panasas.com>

--- osd_initiator/tests/run_par.sh
+++ osd_initiator/tests/run_par.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-MAX_HOSTS=$1
-FILE_NAME=$2
+#PBS -l walltime=180:00:00
+#PBS -l nodes=ib26:ppn=2+ib25:ppn=2+ib21:ppn=2+ib20:ppn=2+ib19:ppn=2+ib18:ppn=2+ib17:ppn=2+ib16:ppn=2+ib15:ppn=2+ib14:ppn=2
 
-if [[ $# -ne 2 ]]; then
-	echo "Need to specify MAX HOSTS and file name";
-	exit 0;
-fi
+export LD_LIBRARY_PATH=/usr/local/mpich2-eth-pvfs-openib/lib:/usr/local/mpich2-eth-pvfs-openib/share
+
+MAX_HOSTS=10
+FILE_NAME=ib
+
+cd /home/dennis/OSD/osd/osd_initiator/tests
 
 echo "Goin to do hosts = 1 to hosts = $MAX_HOSTS";
-echo "Prepending files with $FILE_NAME";
 
 for (( i=1; i<=$MAX_HOSTS; i+=1 )); do
-	/usr/local/bin/mpiexec --pernode -np $i iospeed-mpi > $FILE_NAME.$i
+	/usr/local/bin/mpiexec --pernode -np $i iospeed-mpi
 done
 
 

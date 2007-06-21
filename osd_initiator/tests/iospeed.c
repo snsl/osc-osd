7325784ed533b22032fde30ceb5b0e1ff36ec9ed
Pete Wyckoff <pw@osc.edu>
2007-06-21 21:30:00 +0000

    old git: Option to do just a single size.
    Signed-off-by: Boaz Harrosh <bharrosh@panasas.com>

--- osd_initiator/tests/iospeed.c
+++ osd_initiator/tests/iospeed.c
@@ -196,14 +196,33 @@ static void write_bw(int fd, uint64_t pid, uint64_t oid,
 	free(b);
 }
 
+static void usage(void)
+{
+	fprintf(stderr, "Usage: %s [<size in kbytes>]\n", progname);
+	exit(1);
+}
+
 int main(int argc, char *argv[])
 {
 	int fd, ret, num_drives, i;
 	struct osd_drive_description *drives;
 	const int iter = 500;
 	uint64_t oid;
+	int onesize = 0;
 
 	osd_set_progname(argc, argv); 
+
+	if (argc == 1) {
+		;
+	} else if (argc == 2) {
+		char *cp;
+		onesize = strtoul(argv[1], &cp, 0);
+		if (*cp != '\0')
+			usage();
+		onesize <<= 10;
+	} else {
+		usage();
+	}
 
 	ret = osd_get_drive_list(&drives, &num_drives);
 	if (ret < 0) {
@@ -241,20 +260,25 @@ int main(int argc, char *argv[])
 #endif
  	printf("# osd_initiator/tests/iospeed\n");
 	printf("# type  size (kB)  rate (MB/s) +- stdev\n");
-	for (i=4096; i<(1<<19); i+=4096)
-		write_bw(fd, PARTITION_PID_LB, oid, i, iter, 0);
-	printf("\n\n");
-	for (i=4096; i<(1<<19); i+=4096)
-		read_bw(fd, PARTITION_PID_LB, oid, i, iter, 0);
-	printf("\n\n");
+	if (onesize) {
+		write_bw(fd, PARTITION_PID_LB, oid, onesize, iter, 0);
+		read_bw(fd, PARTITION_PID_LB, oid, onesize, iter, 0);
+	} else {
+		for (i=4096; i<(1<<19); i+=4096)
+			write_bw(fd, PARTITION_PID_LB, oid, i, iter, 0);
+		printf("\n\n");
+		for (i=4096; i<(1<<19); i+=4096)
+			read_bw(fd, PARTITION_PID_LB, oid, i, iter, 0);
+		printf("\n\n");
 #if 0
-	for (i=4096; i<=(1<<19); i+=4096)
-		write_bw(fd, PARTITION_PID_LB, oid, i, iter, 1);
-	printf("\n\n");
-	for (i=4096; i<=(1<<19); i+=4096)
-		read_bw(fd, PARTITION_PID_LB, oid, i, iter, 1);
-	printf("\n\n");
+		for (i=4096; i<=(1<<19); i+=4096)
+			write_bw(fd, PARTITION_PID_LB, oid, i, iter, 1);
+		printf("\n\n");
+		for (i=4096; i<=(1<<19); i+=4096)
+			read_bw(fd, PARTITION_PID_LB, oid, i, iter, 1);
+		printf("\n\n");
 #endif
+	}
 
 	obj_remove(fd, PARTITION_PID_LB, oid);
 

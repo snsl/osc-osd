e80f9ccf025d43a336961560741b5e794a20622f
Ananth Devulapalli <ananth@osc.edu>
2007-06-22 15:53:24 +0000

    old git: run latency test
    Signed-off-by: Boaz Harrosh <bharrosh@panasas.com>

---
+++ osd_initiator/tests/run-lat.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+out=./latency.dat
+bin=./latency
+#bin="echo ran"
+node=ib29
+tcp=10.100.2.69
+ipoib=10.100.5.29
+iser=$ipoib
+updatetcp="--op update -n node.transport_name -v tcp"
+updateiser="--op update -n node.transport_name -v iser"
+
+iscsiadm -m node -T $node -p $tcp:3260 $updatetcp
+iscsiadm -m node -T $node -p $tcp:3260 --login
+echo "# TCP" > $out
+$bin >> $out
+echo >> $out
+iscsiadm -m node -T $node -p $tcp:3260 --logout
+
+
+iscsiadm -m node -T $node -p $ipoib:3260 $updatetcp
+iscsiadm -m node -T $node -p $ipoib:3260 --login
+echo "# IPoIB" >> $out
+$bin >> $out
+echo >> $out
+iscsiadm -m node -T $node -p $ipoib:3260 --logout
+
+iscsiadm -m node -T $node -p $iser:3260 $updateiser
+iscsiadm -m node -T $node -p $iser:3260 --login
+echo "# iSER" >> $out
+$bin >> $out
+echo >> $out
+iscsiadm -m node -T $node -p $iser:3260 --logout

13a2e09f1aa97508304f5b5f9a482873bbdcccab
Pete Wyckoff <pw@osc.edu>
2008-09-16 22:03:35 +0300

    svn r2220: Make mpi version take a command line argument for size.  Do "mpiexec -pernode -n 2 iospeed-mpi 200" e.g.
    SVN Date: 2007-06-28T17:47:56.126688Z
    Auto-commited-by: ./svn_fetch3.sh
    Signed-off-by: Pete Wyckoff <pw@osc.edu>
    Signed-off-by: Boaz Harrosh <bharrosh@panasas.com>

--- osd-initiator/tests/iospeed-mpi.c
+++ osd-initiator/tests/iospeed-mpi.c
@@ -128,50 +128,47 @@ static void read_bw(int fd, uint64_t pid, uint64_t oid,
 	unsigned int j;
 	for (j=0; j<sz; j++) {
 		char *c = (char *)buf + j;
-		if ( c[0] != 'D') {
-			printf("[%d] ERROR READING BUFF (%c)\n", rank, c[0]);
+		if (*c != 'D') {
+			printf("[%d] ERROR READING BUFF (%c)\n", rank, *c);
 		}
 	}
 
+#if 1
+	delta = total_stop - total_start;
+	time = ((double)delta)/mhz; /*time in usec*/
 
-	#if 1
-		delta = total_stop - total_start;
-		time = ((double)delta)/mhz; /*time in usec*/
+	ret = MPI_Reduce(&time, &max_time, 1, MPI_DOUBLE, MPI_MAX,
+			0, MPI_COMM_WORLD);
+	if (ret != MPI_SUCCESS) {
+		printf("MPI ERROR\n");
+	}
 
-		ret = MPI_Reduce(&time, &max_time, 1, MPI_DOUBLE, MPI_MAX,
-				0, MPI_COMM_WORLD);
-		if (ret != MPI_SUCCESS) {
-			printf("MPI ERROR\n");
-		}
+	ret = MPI_Reduce(&time, &min_time, 1, MPI_DOUBLE, MPI_MIN,
+			0, MPI_COMM_WORLD);
+	if (ret != MPI_SUCCESS) {
+		printf("MPI ERROR\n");
+	}
+	if (rank == 0) {
+		total_size = sz * iters * numproc; /*total bytes moved*/
+		printf("read  %3d %3lu %7.3lf --- Discrep %f\n",
+			numproc, sz>>10, total_size/max_time,
+			max_time - min_time);
+	}
+#else
+	mu = mean(b, iters);
+	sd = stddev(b, mu, iters);
 
-		ret = MPI_Reduce(&time, &min_time, 1, MPI_DOUBLE, MPI_MIN,
-				0, MPI_COMM_WORLD);
-		if (ret != MPI_SUCCESS) {
-			printf("MPI ERROR\n");
-		}
-		if (rank == 0) {
-			total_size = sz * iters * numproc; /*total bytes moved*/
-			printf("read %d %3lu %7.3lf --- Discrep %f\n",
-				numproc, sz>>10, total_size/max_time,
-				max_time - min_time);
-		}
-	#else
-		mu = mean(b, iters);
-		sd = stddev(b, mu, iters);
+	total = mu * numproc;
 
-		total = mu * numproc;
-
-		if (dosync)
-			printf("rank %d read-sync  %3lu %7.3lf +- %7.3lf\n",
-			       rank, sz>>10, mu, sd);
-		else
-			printf("rank %d read       %3lu %7.3lf +- %7.3lf\n",
-			       rank, sz>>10, mu, sd);
-		free(buf);
-		free(b);
-	#endif
-
-
+	if (dosync)
+		printf("rank %d read-sync  %3lu %7.3lf +- %7.3lf\n",
+		       rank, sz>>10, mu, sd);
+	else
+		printf("rank %d read       %3lu %7.3lf +- %7.3lf\n",
+		       rank, sz>>10, mu, sd);
+#endif
+	free(buf);
+	free(b);
 }
 
 static void write_bw(int fd, uint64_t pid, uint64_t oid,
@@ -235,41 +232,44 @@ static void write_bw(int fd, uint64_t pid, uint64_t oid,
 	MPI_Barrier(MPI_COMM_WORLD); /*everyone is done reading*/
 	rdtsc(total_stop);
 
-	#if 1
-		delta = total_stop - total_start;
-		time = ((double)delta)/mhz; /*time in usec*/
+#if 1
+	delta = total_stop - total_start;
+	time = ((double)delta)/mhz; /*time in usec*/
 
-		ret = MPI_Reduce(&time, &max_time, 1, MPI_DOUBLE, MPI_MAX,
-				0, MPI_COMM_WORLD);
-		if (ret != MPI_SUCCESS) {
-			printf("MPI ERROR\n");
-		}
-		ret = MPI_Reduce(&time, &min_time, 1, MPI_DOUBLE, MPI_MIN,
-				0, MPI_COMM_WORLD);
-		if (ret != MPI_SUCCESS) {
-			printf("MPI ERROR\n");
-		}
-		if (rank == 0) {
-			total_size = sz * iters * numproc; /*total bytes moved*/
-			printf("write %d %3lu %7.3lf --- Discrep %f\n",
-				numproc, sz>>10, total_size/max_time,
-				max_time - min_time);
-		}
-	#else
+	ret = MPI_Reduce(&time, &max_time, 1, MPI_DOUBLE, MPI_MAX,
+			0, MPI_COMM_WORLD);
+	if (ret != MPI_SUCCESS) {
+		printf("MPI ERROR\n");
+	}
+	ret = MPI_Reduce(&time, &min_time, 1, MPI_DOUBLE, MPI_MIN,
+			0, MPI_COMM_WORLD);
+	if (ret != MPI_SUCCESS) {
+		printf("MPI ERROR\n");
+	}
+	if (rank == 0) {
+		total_size = sz * iters * numproc; /*total bytes moved*/
+		printf("write %3d %3lu %7.3lf --- Discrep %f\n",
+			numproc, sz>>10, total_size/max_time,
+			max_time - min_time);
+	}
+#else
+	mu = mean(b, iters);
+	sd = stddev(b, mu, iters);
+	if (dosync)
+		printf("rank %d write-sync %3lu %7.3lf +- %7.3lf\n",
+		       rank, sz>>10, mu, sd);
+	else
+		printf("rank %d write      %3lu %7.3lf +- %7.3lf\n",
+		       rank, sz>>10, mu, sd);
+#endif
+	free(buf);
+	free(b);
+}
 
-
-		mu = mean(b, iters);
-		sd = stddev(b, mu, iters);
-			if (dosync)
-				printf("rank %d write-sync %3lu %7.3lf +- %7.3lf\n",
-				       rank, sz>>10, mu, sd);
-			else
-				printf("rank %d write      %3lu %7.3lf +- %7.3lf\n",
-				       rank, sz>>10, mu, sd);
-		free(buf);
-		free(b);
-	#endif
-
+static void usage(void)
+{
+	fprintf(stderr, "Usage: %s [<size in kbytes>]\n", progname);
+	exit(1);
 }
 
 int main(int argc, char *argv[])
@@ -278,11 +278,25 @@ int main(int argc, char *argv[])
 	struct osd_drive_description *drives;
 	const int iter = 500;
 	uint64_t oid;
+	int onesize = 0;
 
 	MPI_Init(&argc, &argv);
 	MPI_Comm_size(MPI_COMM_WORLD, &numproc);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	osd_set_progname(argc, argv);
+
+	if (argc == 1) {
+		;
+	} else if (argc == 2) {
+		char *cp;
+		onesize = strtoul(argv[1], &cp, 0);
+		if (*cp != '\0')
+			usage();
+		onesize <<= 10;
+	} else {
+		usage();
+	}
+
 
 	ret = osd_get_drive_list(&drives, &num_drives);
 	if (ret < 0) {
@@ -314,30 +328,43 @@ int main(int argc, char *argv[])
 
 	/* each works on a different object */
 	oid = obj_create_any(fd, PARTITION_PID_LB);
-	//~ for (i=4096; i<=(1<<19); i+=4096) {
-	//~ if (rank == 0) {
-		//~ printf("# osd_initiator/tests/iospeed\n");
-		//~ printf("# type  size (kB)  rate (MB/s) +- stdev\n");
-	//~ }
+
+	if (onesize) {
+		if (rank == 0) {
+			printf("# osd_initiator/tests/iospeed %d\n",
+			       onesize>>10);
+			printf("# type  numproc  size (kB)  rate (MB/s) --- time diff (us)\n");
+		}
 		MPI_Barrier(MPI_COMM_WORLD);
-		write_bw(fd, PARTITION_PID_LB, oid, 204800, iter, 0);
-	//~ }
-	//~ for (i=4096; i<=(1<<19); i+=4096) {
+		write_bw(fd, PARTITION_PID_LB, oid, onesize, iter, 0);
 		MPI_Barrier(MPI_COMM_WORLD);
-		read_bw(fd, PARTITION_PID_LB, oid, 204800, iter, 0);
-	//~ }
+		read_bw(fd, PARTITION_PID_LB, oid, onesize, iter, 0);
+	} else {
+		if (rank == 0) {
+			printf("# osd_initiator/tests/iospeed\n");
+			printf("# type  size (kB)  rate (MB/s) +- stdev\n");
+		}
+		for (i=4096; i<=(1<<19); i+=4096) {
+			MPI_Barrier(MPI_COMM_WORLD);
+			write_bw(fd, PARTITION_PID_LB, oid, i, iter, 0);
+		}
+		for (i=4096; i<=(1<<19); i+=4096) {
+			MPI_Barrier(MPI_COMM_WORLD);
+			read_bw(fd, PARTITION_PID_LB, oid, i, iter, 0);
+		}
 #if 0
-	for (i=4096; i<=(1<<19); i+=4096)
-		write_bw(fd, PARTITION_PID_LB, oid, i, iter, 1);
-	if (rank == 0) {
-		printf("\n\n");
-	}
-	for (i=4096; i<=(1<<19); i+=4096)
-		read_bw(fd, PARTITION_PID_LB, oid, i, iter, 1);
-	if (rank == 0) {
-		printf("\n\n");
-	}
+		for (i=4096; i<=(1<<19); i+=4096)
+			write_bw(fd, PARTITION_PID_LB, oid, i, iter, 1);
+		if (rank == 0) {
+			printf("\n\n");
+		}
+		for (i=4096; i<=(1<<19); i+=4096)
+			read_bw(fd, PARTITION_PID_LB, oid, i, iter, 1);
+		if (rank == 0) {
+			printf("\n\n");
+		}
 #endif
+	}
 
 	obj_remove(fd, PARTITION_PID_LB, oid);
 

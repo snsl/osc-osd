35a796c952c005aea4e3cbd6b4073070117742c6
Pete Wyckoff <pw@osc.edu>
2008-09-16 22:24:44 +0300

    svn r2925: Add GPLv2 license file and copyright headers.
    SVN Date: 2007-12-05T17:57:55.659228Z
    Auto-commited-by: ./svn_fetch3.sh
    Signed-off-by: Pete Wyckoff <pw@osc.edu>
    Signed-off-by: Boaz Harrosh <bharrosh@panasas.com>

--- osd-util/osd-util.h
+++ osd-util/osd-util.h
@@ -1,8 +1,20 @@
 /*
- * Declarations of util.c utilities.
+ * Declarations of osd-util.c utilities.
  *
  * Copyright (C) 2000-7 Pete Wyckoff <pw@osc.edu>
  * Copyright (C) 2007 OSD Team <pvfs-osd@osc.edu>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 2 of the License.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #ifndef __OSD_UTIL_H
 #define __OSD_UTIL_H

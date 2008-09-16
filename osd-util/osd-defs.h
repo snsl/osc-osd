7419645c1cb3586fd4477692f6d15dd9d6f102a0
Dennis Dalessandro <dennis@osc.edu>
2008-09-16 22:36:20 +0300

    svn r2958: change type name
    SVN Date: 2007-12-10T20:54:17.960769Z
    Auto-commited-by: ./svn_fetch3.sh
    Signed-off-by: Dennis Dalessandro <dennis@osc.edu>
    Signed-off-by: Boaz Harrosh <bharrosh@panasas.com>

--- osd-util/osd-defs.h
+++ osd-util/osd-defs.h
@@ -6,12 +6,12 @@
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, version 2 of the License.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -62,7 +62,7 @@
 /*Data Distribution Types*/
 #define DDT_CONTIG	0x0
 #define DDT_SGL		0x1
-#define DDT_IOV		0x2
+#define DDT_VEC		0x2
 #define DDT_RES		0x3
 
 
